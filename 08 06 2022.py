5e7727b7313db6fabdff4508437bb4700a45fdba
FabianIMV <101426902+FabianIMV@users.noreply.github.com>
2022-06-08 20:58:56 -0400

    Add files via upload

---
+++ 08 06 2022.py
@@ -0,0 +1,27 @@
+import numpy as np
+'''matriz = np.array([[0, 1, 2], [3, 4, 5]])
+for f in range (2):
+    for c in range (3):
+        print(matriz[f][c])
+    '''    
+
+
+'''lista = [[1, 2, 3],
+[4, 5, 6]]
+
+matriz = np.array(lista)
+print(matriz)'''
+
+'''matriz = np.array([[0,1,3],[3,4,5]])
+for f in range (2):
+    for c in range (3):
+        print(matriz[f][c])
+print("")
+print(matriz[1,1])'''
+
+matriz = np.array([[0,1,3],[3,4,5]])
+for f in range (2):
+    for c in range (3):
+        print(matriz[f][c])
+print("")
+print(matriz[0,1])
